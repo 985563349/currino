f4bf3b41a67621b24165d964f22a360f2b1a6650
王杰 <jie985563349@outlook.com>
2022-10-25 20:12:14 +0800

    fix: :bug: fix umd,iife module reference errors

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'Formatter',
+      name: 'JeeUtils',
       formats: ['es', 'umd', 'iife'],
     },
   },
