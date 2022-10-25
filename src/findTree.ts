1522bdcfa6b4f31f9f44ffdfbb0ca2a22710cd58
王杰 <jie985563349@outlook.com>
2022-10-25 17:07:24 +0800

    feat: :sparkles: add findTree

---
+++ src/findTree.ts
@@ -0,0 +1,29 @@
+import { forEachRight } from "./forEachRight";
+
+export function findTree<T extends Record<string, any>>(
+  iteratee: (node: T) => boolean | void,
+  tree: T[],
+  childrenKey: string = 'children'
+): T | null {
+  const stack: T[] = [];
+
+  forEachRight((item) => {
+    stack.push(item);
+  }, tree);
+
+  while (stack.length) {
+    const node = stack.pop()!;
+
+    if (iteratee(node)) {
+      return node;
+    }
+
+    if (Array.isArray(node[childrenKey])) {
+      forEachRight((item) => {
+        stack.push(item);
+      }, node[childrenKey] as T[]);
+    }
+  }
+
+  return null;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Box } from "./box";
 export { forEachRight } from "./forEachRight";
+export { findTree } from "./findTree";
 export { sum } from "./sum";
