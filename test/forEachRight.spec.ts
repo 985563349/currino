31e25d77a670f73f189468dfd878d0ffcaa3d634
王杰 <jie985563349@outlook.com>
2023-04-14 11:29:42 +0800

    refactor: optimize code

--- test/forEachRight.spec.ts
+++ test/forEachRight.spec.ts
@@ -2,36 +2,36 @@ import { describe, test, expect } from 'vitest';
 import { forEachRight } from '../src';
 
 describe('forEachRight', () => {
-const list = [{ x: 1, y: 2 }, { x: 3, y: 4 }, { x: 5, y: 6 }];
+  const list = [{ x: 1, y: 2 }, { x: 3, y: 4 }, { x: 5, y: 6 }];
 
-test('performs the passed in function on each element of the list', () => {
-  const sideEffect = {};
+  test('performs the passed in function on each element of the list', () => {
+    const sideEffect = {};
 
-  forEachRight((elem) => {
-    sideEffect[elem.x] = elem.y;
-  }, list);
+    forEachRight((elem) => {
+      sideEffect[elem.x] = elem.y;
+    }, list);
 
-  expect(sideEffect).toStrictEqual({ 5: 6, 3: 4, 1: 2 });
-});
+    expect(sideEffect).toStrictEqual({ 5: 6, 3: 4, 1: 2 });
+  });
 
-test('returns the original list', () => {
-  let s = '';
+  test('returns the original list', () => {
+    let s = '';
 
-  expect(forEachRight((elem) => {
-    s += elem.x;
-  }, list)).toBe(list);
+    expect(forEachRight((elem) => {
+      s += elem.x;
+    }, list)).toBe(list);
 
-  expect(s).toBe('531');
-});
+    expect(s).toBe('531');
+  });
 
-test('early exit of iteration', () => {
-  let s = '';
+  test('early exit of iteration', () => {
+    let s = '';
 
-  forEachRight((elem) => {
-    if (elem.x === 1) return false;
-    s += elem.x;
-  }, list);
+    forEachRight((elem) => {
+      if (elem.x === 1) return false;
+      s += elem.x;
+    }, list);
 
-  expect(s).toBe('53')
-});
+    expect(s).toBe('53')
+  });
 });
