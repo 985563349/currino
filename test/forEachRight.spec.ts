2c4023f91028d244bebc05e470f0fd67c6ff3b66
王杰 <jie985563349@outlook.com>
2023-04-14 10:39:23 +0800

    test: add forEachRight test

---
+++ test/forEachRight.spec.ts
@@ -0,0 +1,37 @@
+import { describe, test, expect } from 'vitest';
+import { forEachRight } from '../src';
+
+describe('forEachRight', () => {
+const list = [{ x: 1, y: 2 }, { x: 3, y: 4 }, { x: 5, y: 6 }];
+
+test('performs the passed in function on each element of the list', () => {
+  const sideEffect = {};
+
+  forEachRight((elem) => {
+    sideEffect[elem.x] = elem.y;
+  }, list);
+
+  expect(sideEffect).toStrictEqual({ 5: 6, 3: 4, 1: 2 });
+});
+
+test('returns the original list', () => {
+  let s = '';
+
+  expect(forEachRight((elem) => {
+    s += elem.x;
+  }, list)).toBe(list);
+
+  expect(s).toBe('531');
+});
+
+test('early exit of iteration', () => {
+  let s = '';
+
+  forEachRight((elem) => {
+    if (elem.x === 1) return false;
+    s += elem.x;
+  }, list);
+
+  expect(s).toBe('53')
+});
+});
