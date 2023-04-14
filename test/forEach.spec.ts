50e40f431d0a836df5525420da683e6bac116eba
王杰 <jie985563349@outlook.com>
2023-04-14 10:33:49 +0800

    test: add forEach test

---
+++ test/forEach.spec.ts
@@ -0,0 +1,37 @@
+import { describe, expect, test } from 'vitest';
+import { forEach } from '../src'
+
+describe('forEach', () => {
+  const list = [{ x: 1, y: 2 }, { x: 3, y: 4 }, { x: 5, y: 6 }];
+
+  test('performs the passed in function on each element of the list', () => {
+    const sideEffect = {};
+
+    forEach((elem) => {
+      sideEffect[elem.x] = elem.y;
+    }, list);
+
+    expect(sideEffect).toStrictEqual({ 1: 2, 3: 4, 5: 6 });
+  });
+
+  test('returns the original list', () => {
+    let s = '';
+
+    expect(forEach((elem) => {
+      s += elem.x;
+    }, list)).toBe(list);
+
+    expect(s).toBe('135');
+  });
+
+  test('early exit of iteration', () => {
+    let s = '';
+
+    forEach((elem) => {
+      if (elem.x === 5) return false;
+      s += elem.x;
+    }, list);
+
+    expect(s).toBe('13')
+  });
+});
